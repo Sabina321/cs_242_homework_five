479172186c0cebe4a895a4562362a00f0a6d000f
ignatndr <ignatndr@gmail.com>
2015-10-27 23:45:50 -0700

    Finished perceptron script

--- perceptron.py
+++ perceptron.py
@@ -2,10 +2,105 @@ import numpy, matplotlib
 
 class Perceptron(object):
     def __init__(self):
-        pass
+        self.dataset = None
+        self.w = numpy.zeros(15)
+        self.taught = False
+        self.weights = []
 
-    def __call__(self):
-        pass
+    def two_epoch_run(self):
+        for i in range(2):
+            for element in self.dataset:
+                prediction = numpy.sign(numpy.dot(self.w, element["data"]))
+
+                if prediction != element["label"]:
+                    self.w += numpy.dot(element["label"], element["data"])
+
+                self.weights.append(self.w)
+
+    def last_hypothesis(self, test_dataset):
+        error_count = 0
+        this_w = self.weights[-1]
+        for element in test_dataset:
+            if element["label"] != numpy.sign(numpy.dot(this_w, element["data"])):
+                error_count += 1
+
+        return error_count
+
+    def average_hypothesis(self, test_dataset):
+        error_count = 0
+        this_w = numpy.sum(self.weights, axis=0)/1000.
+        for element in test_dataset:
+            if element["label"] != numpy.sign(numpy.dot(this_w, element["data"])):
+                error_count += 1
+
+        return error_count
+
+    def voted_hypothesis(self, test_dataset):
+        error_count = 0
+        for element in test_dataset:
+            plus_counts = 0
+            for w in self.weights:
+                if numpy.sign(numpy.dot(w, element["data"])) > 0:
+                    plus_counts += 1
+
+            if plus_counts == 500:
+                error_count += 1
+            elif plus_counts > 500 and element["label"] < 0:
+                error_count += 1
+            elif plus_counts < 500 and element["label"] > 0:
+                error_count += 1
+
+        return error_count
+
+    def last_epoch_average(self, test_dataset):
+        error_count = 0
+        this_w = numpy.sum(self.weights[499:], axis=0)/500.
+        for element in test_dataset:
+            if element["label"] != numpy.sign(numpy.dot(this_w, element["data"])):
+                error_count += 1
+
+        return error_count
+
+    def last_epoch_vote(self, test_dataset):
+        error_count = 0
+        for element in test_dataset:
+            sum = 0
+            for w in self.weights[499:]:
+                sum += numpy.sign(numpy.dot(w, element["data"]))
+
+            vote = numpy.sign(sum)
+            if vote != element["label"]:
+                error_count += 1
+
+        return error_count
+
+
+    def train(self):
+        """ learn over the dataset """
+        # reset the dataset each time
+        self.w = numpy.zeros(15)
+        self.epochs = 0
+        self.errors = {}
+        self.done = False
+
+        while not self.done:
+            error_count = 0
+
+            for element in self.dataset:
+                prediction = numpy.sign(numpy.dot(self.w, element["data"]))
+
+                if prediction != element["label"]:
+                    error_count += 1
+                    self.w += numpy.dot(element["label"], element["data"])
+
+            self.errors["Epoch " + str(self.epochs)] = error_count
+            self.epochs += 1
+
+            if error_count == 0:
+                self.done = True
+
+    def consume_dataset(self, dataset):
+        self.dataset = dataset
 
 
 class DataGenerator(object):
@@ -39,30 +134,86 @@ class DataGenerator(object):
     def generate_a(self):
         for element in self.dataset:
             if element["data"][0] > 0:
-                element["label"] = "+"
+                element["label"] = 1
             else:
-                element["label"] = "-"
+                element["label"] = -1
 
     def generate_b(self):
         """ if at least seven of the features are +1, we label with +1,
         else, we label with -1"""
         for element in self.dataset:
             if sum(element["data"]) >= -1:
-                element["label"] = "+"
+                element["label"] = 1
             else:
-                element["label"] = "-"
+                element["label"] = -1
 
     def generate_c(self):
         for element in self.dataset:
             randomness = 8*numpy.random.random() - 4
             label = randomness + sum(element["data"][:11])
             if label < 0:
-                element["label"] = "-"
+                element["label"] = -1
             else:
-                element["label"] = "+"
+                element["label"] = 1
+
+def a(dg):
+
+    dg.generate_a()
+
+    prcp = Perceptron()
+    prcp.consume_dataset(dg.dataset)
+    prcp.train()
+
+    print prcp.epochs
+
+    print prcp.errors
+
+def b(dg):
+
+    dg.generate_b()
+
+    prcp = Perceptron()
+    prcp.consume_dataset(dg.dataset)
+    prcp.train()
+
+    print prcp.epochs
+
+    print prcp.errors
+
+
+def c(dg):
+    dg.generate_c()
+
+    prcp = Perceptron()
+    prcp.consume_dataset(dg.dataset)
+    prcp.two_epoch_run()
+
+    test_set = DataGenerator()
+    test_set.generate_dataset()
+    test_set.generate_c()
+
+    print "Last hypothesis"
+    print prcp.last_hypothesis(test_set.dataset)
+
+    print "Average hypothesis"
+    print prcp.average_hypothesis(test_set.dataset)
+
+    print "Voted hypothesis"
+    print prcp.voted_hypothesis(test_set.dataset)
+
+    print "Last epoch average"
+    print prcp.last_epoch_average(test_set.dataset)
+
+    print "Last epoch vote"
+    print prcp.last_epoch_vote(test_set.dataset)
 
 
 if __name__ == '__main__':
     dg = DataGenerator()
     dg.generate_dataset()
-    print dg.dataset
+
+    # a(dg)
+
+    # b(dg)
+
+    c(dg)
